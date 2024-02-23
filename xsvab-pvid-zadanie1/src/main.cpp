8d7a772ee737914be3cb383f33fa121ba37bf4c9
xsvab@stuba.sk <martin.svab@gmail.com>
2024-02-23 16:39:23 +0100

    finished task???

--- xsvab-pvid-zadanie1/src/main.cpp
+++ xsvab-pvid-zadanie1/src/main.cpp
@@ -2,11 +2,67 @@
 #include <opencv2/opencv.hpp>
 
 
+void detectObjects(cv::Mat input, cv::Mat processed, int objectSize, int retMode)
+{
+	cv::Mat output = input.clone();
+	int objectCount = 0;
+
+	std::vector<std::vector<cv::Point>> contours;
+	cv::findContours(processed, contours, retMode, cv::CHAIN_APPROX_SIMPLE);
+
+	for (int i = 0; i < contours.size(); ++i) {
+		std::vector<cv::Point> points = contours[i];
+
+		if (cv::contourArea(points) > objectSize) {
+			objectCount++;
+
+			double centerX = 0.0;
+			double centerY = 0.0;
+
+			for (cv::Point point : points) {
+				centerX += point.x;
+				centerY += point.y;
+			}
+
+			centerX /= points.size();
+			centerY /= points.size();
+
+			int fontFace = cv::FONT_HERSHEY_PLAIN;
+			double fontScale = 0.5;
+			cv::Point fontPoint = cv::Point(centerX, centerY);
+			cv::Scalar color = cv::Scalar(0, 0, 255);
+
+			cv::putText(output, std::to_string(objectCount), fontPoint, fontFace, fontScale, color);
+			cv::drawContours(output, contours, i, color);
+		}
+	}
+
+	cv::imshow("output", output);
+	std::cout << "Detected " << objectCount << " objects." << std::endl;
+}
+
 void countCancerCells()
 {
-	cv::Mat image = cv::imread("assets\\Histo_CV2.png");
-	cv::imshow("original", image);
+	// input
 
+	cv::Mat input = cv::imread("assets\\Histo_CV2.png");
+	cv::resize(input, input, cv::Size(input.cols / 8, input.rows / 8));
+	cv::imshow("input", input);
+
+	// preprocessing
+
+	cv::Mat grayscale;
+	cv::cvtColor(input, grayscale, cv::COLOR_BGR2GRAY);
+	cv::imshow("grayscale", grayscale);
+
+	// binarization
+
+	cv::Mat threshold;
+	cv::threshold(grayscale, threshold, 100, 255, cv::THRESH_BINARY);
+	cv::bitwise_not(threshold, threshold);
+	cv::imshow("threshold", threshold);
+
+	detectObjects(input, threshold, -1, cv::RETR_EXTERNAL);
 	cv::waitKey();
 }
 
@@ -39,60 +95,52 @@ void countBeerBubbles()
 
 	// contour detection
 
-	cv::Mat output = input.clone();
-	int objectSize = 50;
-	int objectCount = 0;
-
-	std::vector<std::vector<cv::Point>> contours;
-	cv::findContours(dilate, contours, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE);
-
-	for (int i = 0; i < contours.size(); ++i) {
-		std::vector<cv::Point> points = contours[i];
-
-		if (cv::contourArea(points) > objectSize) {
-			objectCount++;
-
-			double centerX = 0.0;
-			double centerY = 0.0;
-
-			for (cv::Point point : points) {
-				centerX += point.x;
-				centerY += point.y;
-			}
-
-			centerX /= points.size();
-			centerY /= points.size();
-
-			int fontFace = cv::FONT_HERSHEY_PLAIN;
-			double fontScale = 0.5;
-			cv::Point fontPoint = cv::Point(centerX, centerY);
-			cv::Scalar color = cv::Scalar(0, 0, 255);
-
-			cv::putText(output, std::to_string(objectCount), fontPoint, fontFace, fontScale, color);
-			cv::drawContours(output, contours, i, color);
-		}
-	}
-
-	// output
-
-	cv::imshow("output", output);
-	std::cout << "Detected " << objectCount << " beer bubbles." << std::endl;
+	detectObjects(input, dilate, 50, cv::RETR_CCOMP);
 	cv::waitKey();
 }
 
 void countBloodCells()
 {
-	cv::Mat image = cv::imread("assets\\Red_blood_cells\\img (1).png");
-	cv::imshow("original", image);
+	// input
 
+	cv::Mat input = cv::imread("assets\\Red_blood_cells\\img (1).png");
+	cv::imshow("original", input);
+
+	//  preprocessing
+
+	cv::Mat grayscale;
+	cv::cvtColor(input, grayscale, cv::COLOR_BGR2GRAY);
+	cv::imshow("grayscale", grayscale);
+
+	cv::Mat gauss;
+	cv::GaussianBlur(grayscale, gauss, cv::Size(5, 5), 0);
+	cv::imshow("gauss", gauss);
+
+	// binarization
+
+	cv::Mat threshold;
+	cv::threshold(gauss, threshold, 200, 255, cv::THRESH_BINARY);
+	cv::bitwise_not(threshold, threshold);
+	cv::imshow("threshold", threshold);
+
+	// postprocessing
+
+	cv::Mat erode;
+	cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5));
+	cv::erode(threshold, erode, kernel);
+	cv::imshow("erode", erode);
+
+	// contour detection
+
+	detectObjects(input, threshold, 20, cv::RETR_EXTERNAL);
 	cv::waitKey();
 }
 
 int main()
 {
-	//countCancerCells();
+	countCancerCells();
 	countBeerBubbles();
-	//countBloodCells();
+	countBloodCells();
 
 	return 0;
 }
